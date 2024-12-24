bbaae8a76b72d171eddfa171bbba7f78f5497109
Rai Akmal <muhammadrai0504@gmail.com>
2024-12-24 17:51:08 +0700

    add about

--- app/(front-end)/page.js
+++ app/(front-end)/page.js
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import HeroSection from "@/components/frontend/HeroSection";
+import AboutSection from "@/components/frontend/AboutSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       {/*  <LoginLink>Sign in</LoginLink>*/}
       {/*</Button>*/}
       <HeroSection/>
+      <AboutSection/>
     </div>
   );
 }
