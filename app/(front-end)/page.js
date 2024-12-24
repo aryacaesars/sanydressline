55558e60b859211df539c4f58bdc3c11b98273cd
kudith <ginanjar.aditya15@gmail.com>
2024-12-24 14:37:45 +0700

    add navbar component

--- app/(front-end)/page.js
+++ app/(front-end)/page.js
@@ -4,10 +4,10 @@ import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <Button>
-        <LoginLink>Sign in</LoginLink>
-      </Button>
+      {/*<h1>Home</h1>*/}
+      {/*<Button>*/}
+      {/*  <LoginLink>Sign in</LoginLink>*/}
+      {/*</Button>*/}
     </div>
   );
 }
